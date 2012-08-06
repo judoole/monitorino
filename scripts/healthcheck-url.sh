d7a457ac191381c06b8df44294e853e6e04727f1
Ole Christian LangfjÃ¦ran <judoole@gmail.com>
2012-08-06 15:06:06 +0200

    Jenkins script for healtcheck

---
+++ scripts/healthcheck-url.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "usage: healthcheck.sh <url>"
+    exit 1
+fi
+
+status=`curl --silent --head $1 | head -1 | cut -f 2 -d' '`
+
+if [ "$status" != "200" ]
+then
+    echo "status was other than '200': was '$status'"
+    exit 1
+fi
+
+if [ ! -e "reports" ]
+then
+  mkdir "reports"
+fi
+
+#put the xml version of the page into a file
+curl --silent $1 > reports/healthcheck.xml
